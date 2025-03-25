5acbc6b582680fc6cfbf1dff04178ca567d64091
Amit Kumar <84862054+Amit-kumar97@users.noreply.github.com>
2025-03-25 23:28:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const byteSize = (str) => {
   // write your code here
+	// let encoder = new TextEncoder();
+	// return encoder.encode(str).length;
+
+	let byteCount = 0;
+	for (let i = 0; i < str.length; i++) {
+	    const charCode = str.charCodeAt(i);
+	    if (charCode < 128) {
+	      byteCount++;
+	    } else if (charCode < 2048) {
+	      byteCount += 2;
+	    } else if (charCode < 55296 || charCode >= 57344) {
+	      byteCount += 3;
+	    } else {
+	      // Surrogate pair (emoji, etc.)
+	      byteCount += 4;
+	      i++; // Skip the next code unit
+	    }
+	  }
+	  return byteCount;
 };
 
 // Do not change the code below
